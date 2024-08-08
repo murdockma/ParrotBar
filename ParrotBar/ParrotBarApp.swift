3a3cd28293e510fe692c204c1f17d5818778201e
murdockma <murdockmichael2014@gmail.com>
2024-08-07 23:57:50 -0600

    Initial Commit

---
+++ ParrotBar/ParrotBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParrotBarApp.swift
+//  ParrotBar
+//
+//  Created by michael on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ParrotBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
